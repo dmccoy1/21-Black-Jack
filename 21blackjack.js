c12724a753ebf4274e9f4d321e65509d0f104748
Darius McCoy <dariusmccoy@Betamores-Air-2.daintytoaster.com>
2022-05-12 15:03:17 -0400

    updated code

--- 21blackjack.js
+++ 21blackjack.js
@@ -1,4 +1,4 @@
-const prompt = require("prompt-sync")(); //needed to take input from the console. 
+const prompt = require("prompt-sync")();
 
 class Deck {
     constructor() {
@@ -6,8 +6,9 @@ class Deck {
         this.ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
     }
 
-    makeDeck() {//Loop combines a rank to each suit 
+    makeDeck() {
         let deck = [];
+
         for (let i = 0; i < this.suits.length; i++) {
             for (let rank = 0; rank < this.ranks.length; rank++) {
                 deck.push(`${this.ranks[rank]} of ${this.suits[i]}`)
@@ -18,63 +19,56 @@ class Deck {
     }
 
     shuffle(deck) {
-        for (let i = 0; i < 52; i++) {
-            let card = deck[i];
-            let shuffle = Math.floor(Math.random() * 52);
-            deck[i] = deck[shuffle];
-            deck[shuffle] = card;
+        console.log("shuffling deck")
+        let m = deck.length
+        let i;
+        while (m) {
+            i = Math.floor(Math.random() * m--);
+            [deck[m], deck[i]] = [deck[i], deck[m]];
         }
         return deck
     }
 }
+
 class Player {
     constructor() {
         this.hand = []
         this.money = 1000
-        this.playAgain = true
+        this.playAgain = false
         this.wager = 0
         this.score = 0
         this.hitOrStay = "y"
     }
-    bet() {//a little input validation!
-        this.wager = Number(prompt(`How much do you want to bet? You currently have $${this.money} in your bank `))
+    makeWager() {
         while (true) {
+            this.wager = Number(prompt("How much do you want to bet? "));
             if (isNaN(this.wager)) {
-                console.log(`Use intergers to place your bet! (0-9)`)
+                console.log(`Please enter a digit less than less than ${this.money}`)
             } else if (this.wager > this.money) {
-                console.log(`Don't be silly place a bet that's not more than what you have: $${this.money}`)
+                console.log(`Please enter a digit less than less than ${this.money}`)
                 continue
             } else {
                 break
             }
-            console.log(`Player bet ${this.wager}.`)
-            this.money -= this.wager;
         }
 
+        console.log(`Player bet ${this.wager}.`)
         this.money -= this.wager
     }
 
-    drawCard(n) {
-        let hand = this.hand
-        for (let i = 0; i < n; i++) {
-            let card = deck.pop()
-            this.addCardValue(card)
-            hand.push(card)
-        }
+    stay() {
     }
-    openhand() {//player is drawn 2 cards at the start of each round
-        this.drawCard(2)
-    }
-    hit() {
+
+    hit(deck) {
         if (this.hand.length >= 2) {
             while (true) {
-                this.hitOrStay = prompt("Do you want to hit?(y/n) ")
+                this.hitOrStay = prompt("Do you want to hit (y/n)? ")
                 if (this.hitOrStay === "y") {
-                    console.log("Player draws a card...")
-                    this.drawCard(1)
-                    console.log("You were dealt a new card " + this.hand[this.hand.length - 1])
-                    console.log(`You currently have ${this.hand} in your hand.`)
-                    console.log(`Your score is now: ${this.score}.`)
+                    console.log("Player is dealt a card...")
+                    let newCard = deck.pop()
+                    this.addCardValue(newCard)
+                    console.log(`Player was dealt a ${newCard}. Player score is now ${this.score}.`)
+                    this.hand.push(newCard)
                 } else if (this.hitOrStay !== "y" && this.hitOrStay !== "n") {
                     console.log("Please enter 'y' or 'n'")
                     continue
@@ -82,12 +76,18 @@ class Player {
                     break
                 }
             }
+
         } else {
-            console.log("Player draws a card...")
-            this.drawCard(1)
+            console.log("Player is dealt a card...")
+            let newCard = deck.pop()
+            this.addCardValue(newCard)
+            this.hand.push(newCard)
         }
     }
-
+    dealOpeningHand(deck) {
+        this.hit(deck)
+        this.hit(deck)
+    }
     showHand() {
         console.log("The players hand is the following: ")
         console.log(this.hand)
@@ -98,11 +98,8 @@ class Player {
         this.hitOrStay = "y"
         this.score = 0
     }
-    showScore() {
-        console.log("The player's score is: " + this.score)
-    }
 
-    addCardValue(card) {//score is calculated - Ace is either a 1 or 11 based on score when drawn 
+    addCardValue(card) {
         let cardRank = card.split(" ")[0]
         if (parseInt(cardRank) <= 10) {
             this.score += parseInt(cardRank)
@@ -122,21 +119,24 @@ class Dealer {
         this.hand = []
         this.score = 0
     }
-    drawCard(n) {
-        let hand = this.hand
-        for (let i = 0; i < n; i++) {
-            let card = deck.pop()
-            this.addCardValue(card)
-            hand.push(card)
-        }
 
+    hit(deck) {
+        if (this.hand.length >= 2) {
+            console.log("Dealer is dealt a card...")
+            let newCard = deck.pop()
+            this.addCardValue(newCard)
+            console.log(`Dealer was dealt a ${newCard}. Dealer score is now ${this.score}.`)
+            this.hand.push(newCard)
+        } else {
+            console.log("Dealer is dealt a card...")
+            let newCard = deck.pop()
+            this.addCardValue(newCard)
+            this.hand.push(newCard)
+        }
     }
-    hit() {
-        console.log("Dealer draws a card...")
-        this.drawCard(1)
-    }
-    openhand() {//the dealer is drawn two cards but the top card is only reavled below
-        this.drawCard(2)
+    dealOpeningHand(deck) {
+        this.hit(deck)
+        this.hit(deck)
     }
 
     showHand() {
@@ -145,7 +145,7 @@ class Dealer {
     }
 
     showFullHand() {
-        console.log("The dealer is showing their full hand... ")
+        console.log("The dealer is showing the following: ")
         console.log(this.hand)
     }
 
@@ -171,115 +171,88 @@ class Dealer {
         }
     }
 }
-let player = new Player()
-let dealer = new Dealer()
-let deck = new Deck()
-deck = deck.makeDeck()
-function greet() {
-    console.log("Ayo lads and lassies! It's famous actor Jack Black here!");
-    console.log("How about I dabble you in a game of Black Jack")
-}
-function gameIntro() {
-    while (true) {
-        let welcome = prompt("Do you want to play? (Y/N)");
-        if (welcome == "Y" || welcome == "y") {
-            console.log("Starting game now!")
-            playGame()
-        }
-        else if (welcome !== "Y" && welcome !== "y" && welcome !== "N" && welcome !== "n") {
-            console.log("You must enter 'y' to play!")
-            gameIntro()
-            continue
-        }
-        else if (welcome == "N" || welcome == "n") {
-            console.log("You Suck!")
-            player.playAgain = false
-            return false
-        } else {
-            break
-        }
-    }
-}
 function playGame() {
+    console.log("Welcome to Jack Blacks BLACK JACK!")
 
     // Create deck of cards
+    let deckObject = new Deck()
+    let gameDeck = deckObject.makeDeck()
+
     // Create player & dealer
-    while (player.playAgain = true) {
-        while (player.money > 0) {//game will run aslong as player has money 
-            // Player needs to bet
-            player.bet()
-            // Deal the cards
-            console.log("Dealing the players cards...")
-            player.openhand()
-            if (player.score == 21) {
-                console.log("BLACKJACK!!!")
-                console.log("Player WINS")
-                player.money += player.wager * 2
-                console.log(`Player has $${player.money}.`)
-                player.reset()
-                dealer.reset()
-                continue
-            }
-            //console.log("The player is showing the following: ")
-            player.showHand()
+    let player = new Player()
+    let dealer = new Dealer()
+    console.log(`We're all ready to go. You currently have ${player.money}.`)
 
-            console.log("Dealing the dealers cards...")
-            dealer.openhand()
-            //console.log("The dealer is showing the following: ")
-            dealer.showHand()
+    while (player.money > 0) {
+        // Player needs to bet
+        player.makeWager()
 
-            // Player plays game
-            while (player.hitOrStay === "y") {
-                player.hit()
-
-                if (player.score > 21) {
-                    console.log("BUST...YOU LOST...LOSER")
-                    console.log(`You have ${player.money} left.`)
-                    break
-                }
-            }
-
-            if (player.score > 21) {
-                player.reset()
-                dealer.reset()
-                continue
-            }
-            console.log(`Player stays with a score of ${player.score}.`)
-            console.log("Dealers Turn!")
-
-            // Dealer plays 
-            console.log("Here comes the dealers Big Reveal!")
-            dealer.showFullHand()
-
-            while (dealer.score < 17) {
-                dealer.hit()
-            }
-            if (dealer.score > 21) {
-                console.log("Dealer BUSTS")
-                console.log("Player WINS")
-                player.money += player.wager * 2
-                console.log(`Player has $${player.money}.`)
-                player.reset()
-                dealer.reset()
-                continue
-            }
-            if (player.score > dealer.score) {
-                console.log("Player WINS")
-                player.money += player.wager * 2
-                console.log(`Player now has $${player.money}.`)
-            } else {
-                console.log("Dealer WINS")
-                console.log(`Player now has $${player.money}.`)
-
-            }
-
+        // Deal the cards
+        console.log("Dealing the players cards...")
+        player.dealOpeningHand(gameDeck)
+        if (player.score == 21) {
+            console.log("BLACKJACK!!!")
+            console.log("Player WINS")
+            player.money += player.wager * 2
+            console.log(`Player has $${player.money}.`)
             player.reset()
             dealer.reset()
+            continue
+        }
+        player.showHand()
+
+        console.log("Dealing the dealers cards...")
+        dealer.dealOpeningHand(gameDeck)
+        dealer.showHand()
+
+        while (player.hitOrStay === "y") {
+            player.hit(gameDeck)
+
+            if (player.score > 21) {
+                console.log("BUST...YOU LOST...LOSER")
+                console.log(`You have ${player.money} left.`)
+                break
+            }
+        }
+
+        if (player.score > 21) {
+            player.reset()
+            dealer.reset()
+            continue
+        }
+        console.log(`Player stays with a score of ${player.score}.`)
+        console.log("Dealers Turn!")
+
+        console.log("Here comes the dealers Big Reveal!")
+        dealer.showFullHand()
+
+        while (dealer.score < 17) {
+            dealer.hit(gameDeck)
+        }
+        if (dealer.score > 21) {
+            console.log("Dealer BUSTS")
+            console.log("Player WINS")
+            player.money += player.wager * 2
+            console.log(`Player has $${player.money}.`)
+            player.reset()
+            dealer.reset()
+            continue
+        }
+        if (player.score > dealer.score) {
+            console.log("Player WINS")
+            player.money += player.wager * 2
+            console.log(`Player now has $${player.money}.`)
+        } else {
+            console.log("Dealer WINS")
+            console.log(`Player now has $${player.money}.`)
 
         }
+
+        player.reset()
+        dealer.reset()
+
     }
 }
-greet()
-gameIntro()
-
-
+console.log("Ayo lads and lassies! It's famous actor Jack Black here!");
+console.log("How about I dabble you in a game of Black Jack")
+playGame()
